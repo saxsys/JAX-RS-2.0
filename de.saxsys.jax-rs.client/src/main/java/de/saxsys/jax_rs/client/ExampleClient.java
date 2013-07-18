dd2153c1eebe763b6a4ca854d19be5969ac86209
Stefan Bley <stefan.bley@saxsys.de>
2013-07-18 20:41:47 +0200

    Client with /user/1 and reading xml to string

--- de.saxsys.jax-rs.client/src/main/java/de/saxsys/jax_rs/client/ExampleClient.java
+++ de.saxsys.jax-rs.client/src/main/java/de/saxsys/jax_rs/client/ExampleClient.java
@@ -1,9 +1,5 @@
 package de.saxsys.jax_rs.client;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.URI;
 
 import javax.ws.rs.client.Client;
@@ -35,19 +31,8 @@ public class ExampleClient {
 		// GET
 		Response response = userTarget.request(MediaType.APPLICATION_XML).get();
 
-		InputStream is = response.readEntity(InputStream.class);
-		printToStdOut(is);
+		String userXml = response.readEntity(String.class);
+		System.out.println(userXml);
 	}
 
-	private static void printToStdOut(InputStream is) {
-		BufferedReader in = new BufferedReader(new InputStreamReader(is));
-
-		String inputLine;
-		try {
-			while ((inputLine = in.readLine()) != null)
-				System.out.println(inputLine);
-			in.close();
-		} catch (IOException e) {
-		}
-	}
 }
